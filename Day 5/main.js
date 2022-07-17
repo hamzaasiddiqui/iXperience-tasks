e7a15cc69d4f0f3cf9ec204aaf885dc0f77bb9a6
=hamzasiddiqui <=hamza.eins@gmail.com>
2022-07-17 23:59:19 +0500

    added comments to \Day 5\main.js"

--- Day 5/main.js
+++ Day 5/main.js
@@ -14,6 +14,7 @@ class UserInterface {
         this.tasks = [];
     }
 
+    //function to add event listener to form
     bindEventListener() {
         this.form.addEventListener('submit', (e) => this.onFromSubmit(e));
     }
@@ -35,6 +36,7 @@ class UserInterface {
         this.tableBody.innerHTML = '';
 
         for (const task of this.tasks) {
+            //creating elements for table
             const row = document.createElement('tr');
 
             const taskCell = document.createElement('td');
@@ -44,7 +46,9 @@ class UserInterface {
             const removeButton = document.createElement('button');
             const completedButton = document.createElement('button');
 
+            //adding innerHTML to elements
             taskCell.innerHTML = task.description;
+            //to add button to complete a task
             if (task.completed == false) {
                 completedCell.innerHTML = 'No';
                 completedButton.innerHTML = '✓';
@@ -52,7 +56,9 @@ class UserInterface {
                 completedButton.classList.add('btn');
                 completedButton.classList.add('btn-outline-success');
                 completedButton.classList.add('me-3');
-            } else {
+            }
+            //to add a button to undo completed status of task
+            else {
                 completedCell.innerHTML = 'Yes';
                 completedButton.innerHTML = '⟲';
 
@@ -65,9 +71,12 @@ class UserInterface {
             removeButton.classList.add('btn');
             removeButton.classList.add('btn-outline-danger');
 
+            //adding event listener to completed button
             if (task.completed == false) {
                 completedButton.addEventListener('click', (e) => this.onCompletedButtonClick(task));
-            } else {
+            }
+            //adding event listener to undo button
+            else {
                 completedButton.addEventListener('click', (e) => this.onUndoCompletedButtonClick(task));
             }
             
@@ -84,28 +93,36 @@ class UserInterface {
     }
 
     onCompletedButtonClick(completeTask) {
+        //updating completed status of task
         let completedTask = new Task(completeTask.description);
         completedTask.completed = true;
 
+        //filtering out the task with outdated completed status
         this.tasks = this.tasks.filter((task) => {
             return task.description != completeTask.description;
         });
 
+        //pushing task with updated status to tasks array
         this.tasks.push(completedTask);
         
+        //calling function to populate table again
         this.populateTasksTable();
     }
 
     onUndoCompletedButtonClick(completedTask) {
+        //updating completed status of task
         let undoTask = new Task(completedTask.description);
         undoTask.completed = false;
 
+        //filtering out the task with outdated completed status
         this.tasks = this.tasks.filter((task) => {
             return task.description != completedTask.description;
         });
         
+        //pushing task with updated status to tasks array
         this.tasks.push(undoTask);
 
+        //calling function to populate table again
         this.populateTasksTable();
     }
 
